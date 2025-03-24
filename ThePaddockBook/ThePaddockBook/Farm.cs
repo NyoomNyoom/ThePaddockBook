fccc68422e9efba4cb855baec295a2cfadfd313b
NyoomNyoom <jacksonnorth1275@gmail.com>
2025-03-24 19:34:02 +1300

    Working on the Farm class

--- ThePaddockBook/ThePaddockBook/Farm.cs
+++ ThePaddockBook/ThePaddockBook/Farm.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace ThePaddockBook
 {
-    class Farm
+    class Farm(String nameIn)
     {
+        private readonly String farmName = nameIn;
+        private readonly List<Location> locations = new();
+        private readonly List<User> employees = new();
+
+        public void AddLocation(Location LocoIn)
+        {
+            locations.Add(LocoIn);
+        }
+
+        public void AddEmployee(User employee)
+        {
+            employees.Add(employee);
+        }
     }
 }
